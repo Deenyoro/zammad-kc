f06dacea0884695d1072167f0bda3424f72d33ef
Deenyoro <dean@kawaconnect.com>
2026-02-03 14:28:38 -0500

    fix: correct origin_by_id sender override bug
    Zammad has a bug comparing User object to integer ID which always
    forces sender to Customer when origin_by_id is set. This breaks
    Teams/SMS delivery since communicate jobs require sender=Agent.
    
    Adds KC concern to override the buggy callback with correct ID
    comparison.

--- kc/config/initializers/kc_loader.rb
+++ kc/config/initializers/kc_loader.rb
@@ -61,6 +61,7 @@ Rails.application.config.to_prepare do
   kc_prepend.call('Ticket::TimeAccounting', 'Kc::TimeAccountingAgent')
   kc_prepend.call('Ticket::Article', 'Kc::EnqueueCommunicateTeamsChatJob')
   kc_prepend.call('Ticket::Article', 'Kc::EnqueueCommunicateRingcentralSmsJob')
+  kc_prepend.call('Ticket::Article', 'Kc::FixArticleOriginBySender')
 
   Rails.logger.info 'KC: Overlay loading complete.'
 end
